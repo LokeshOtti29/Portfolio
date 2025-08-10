0293aa4e5f4375918181ece32b1764f1befefa6f
LokeshOtti29 <lokeshotti456@gmail.com>
2025-08-10 23:49:01 +0530

    changing-the-folder-structure

--- src/app/About/Page.tsx
+++ src/app/About/Page.tsx
@@ -1,8 +1,7 @@
-
 const About = () => {
   return (
     <div>
-      about
+      
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import About from "./components/About";
-import Navbar from "./components/Navbar";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-6 m-4">
       <header>
-        <Navbar />
+       
       </header>
       <main className="flex justify-center">
-        <About />
+        
       </main>
       <footer></footer>
     </div>
